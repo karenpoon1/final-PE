1f487405cdf85f91e3b72c17b0408650939ef64f
Karen Poon <poonyanlam@gmail.com>
2022-05-16 17:27:51 +0100

    load saved model results from file

---
+++ src/utils/load_result.py
@@ -0,0 +1,9 @@
+import torch
+
+from utils.get_pathname import get_pathname
+
+def load_result(split_config, model_config, additional):
+    raw_pathname, _ = get_pathname(split_config, model_config, additional)
+    print(raw_pathname)
+    results = torch.load(raw_pathname)
+    return results

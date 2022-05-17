336d638ad0300d3f539b446217b6834fcbac80fd
Karen Poon <poonyanlam@gmail.com>
2022-05-17 11:49:38 +0100

    remove unnecessary print

--- src/utils/load_result.py
+++ src/utils/load_result.py
@@ -4,6 +4,5 @@ from utils.get_pathname import get_pathname
 
 def load_result(split_config, model_config, additional):
     raw_pathname, _ = get_pathname(split_config, model_config, additional)
-    print(raw_pathname)
     results = torch.load(raw_pathname)
     return results

ceac417fd61e1d0b8b57de6da7aa1f44ad5a7f66
Karen Poon <poonyanlam@gmail.com>
2022-05-19 14:06:35 +0100

    load results from simulated datasets experiments

--- src/utils/load_result.py
+++ src/utils/load_result.py
@@ -1,8 +1,21 @@
 import torch
 
-from utils.get_pathname import get_pathname
+from utils.get_pathname import get_pathname, get_pathname_sim, get_data_pathname_sim
+
 
 def load_result(split_config, model_config, additional):
     raw_pathname, _ = get_pathname(split_config, model_config, additional)
     results = torch.load(raw_pathname)
     return results
+
+
+def load_result_sim(split_config, model_config, synthetic_config, additional):
+    raw_pathname, _ = get_pathname_sim(split_config, model_config, synthetic_config, additional)
+    results = torch.load(raw_pathname)
+    return results
+
+
+def load_data_sim(synthetic_config, additional):
+    raw_pathname = get_data_pathname_sim(synthetic_config, additional)
+    data = torch.load(raw_pathname)
+    return data
